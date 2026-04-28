c2ba7c459ace944240676f2f919b7b8f06197460
B0UGDX <ratonrangelmejia+2@gmail.com>
2026-04-27 23:50:17 -0600

    feat: filtros

---
+++ .open-next/server-functions/default/.next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/03~yq9q893hmn.js"
+  ],
+  "lowPriorityFiles": [
+    "static/3ZuSiPshbgAwuNWT5VW3J/_buildManifest.js",
+    "static/3ZuSiPshbgAwuNWT5VW3J/_ssgManifest.js",
+    "static/3ZuSiPshbgAwuNWT5VW3J/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/00gq-v0e07i1q.js",
+    "static/chunks/0pqt~8bl3ukh4.js",
+    "static/chunks/0257pdz1-imal.js",
+    "static/chunks/0n~dq4kpx9xxx.js",
+    "static/chunks/turbopack-184q9f1zvtbok.js"
+  ]
+};
